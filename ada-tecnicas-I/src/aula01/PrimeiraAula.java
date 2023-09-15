aa3cd2d4d0e1f1333324eeacab720aeb5f015e9d
Helen Her <her.helen@gmail.com>
2023-09-15 20:52:24 -0300

    add more code about date in Java

--- ada-tecnicas-I/src/aula01/PrimeiraAula.java
+++ ada-tecnicas-I/src/aula01/PrimeiraAula.java
@@ -1,8 +1,6 @@
 package aula01;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.time.*;
 
 public class PrimeiraAula {
     public static void main(String[] args) {
@@ -17,11 +15,33 @@ public class PrimeiraAula {
         LocalTime agora = LocalTime.now();
         System.out.println(agora);
 
-        // impeime a data e a hora atual
+        // imprime a data e a hora atual
+        // 2023-09-15T20:27:08.018628300 => T
         LocalDateTime diaHora = LocalDateTime.now();
         System.out.println(diaHora);
 
-        
+        // imprime uma data escolhida
+        LocalDate dataEscolhida = LocalDate.of(2021, 1, 31);
+        System.out.println(dataEscolhida);
+//        LocalDate dataErrada = LocalDate.of(2023, Month.FEBRUARY, 30);
+
+        // imprime 50o dia do ano de 2023
+        LocalDate diaDoAno = LocalDate.ofYearDay(2023, 50);
+        System.out.println(diaDoAno);
+
+        LocalTime horaMinuto = LocalTime.of(20, 30);
+        System.out.println(horaMinuto);
+        LocalTime horaMinutoSegundo = LocalTime.of(20, 30, 35);
+        System.out.println(horaMinutoSegundo);
+
+//        System.out.println(LocalTime.of(23, 60)); // minutos fora do limite
+
+        LocalDateTime anoMesHoraMinuto = LocalDateTime.of(2023, Month.SEPTEMBER, 15,
+                20, 49, 20);
+        System.out.println(anoMesHoraMinuto);
+
+        LocalDateTime dataHoraPersonalizada = LocalDateTime.of(hoje, horaMinutoSegundo);
+        System.out.println(dataHoraPersonalizada);
 
     }
 }
