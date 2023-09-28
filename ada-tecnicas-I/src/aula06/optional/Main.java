b5de98e9fb389463711f3bf080713ec0a507cf60
Helen Her <her.helen@gmail.com>
2023-09-27 21:27:51 -0300

    example of Optional.ifPresentOrElse usage

--- ada-tecnicas-I/src/aula06/optional/Main.java
+++ ada-tecnicas-I/src/aula06/optional/Main.java
@@ -7,14 +7,22 @@ public class Main {
     public static void main(String[] args) {
         Database db = new Database();
         Optional<User> searchedUser = db.getByLogin("login 3");
-        printUserLogin(searchedUser);
+        printUserLoginOptional(searchedUser);
+        searchedUser.ifPresentOrElse(
+            user -> printUserLogin(user),
+            () -> System.out.println("ifPresentOrElse: User not found.")
+        );
     }
 
-    public static void printUserLogin(Optional<User> user) {
+    public static void printUserLogin(User user) {
+        System.out.println(user.getLogin());
+    }
+
+    public static void printUserLoginOptional(Optional<User> user) {
         if (user.isPresent()) {
             System.out.println(user.get().getLogin());
         } else {
-            System.out.println("User not found.");
+            System.out.println("printUserLoginOptional: User not found.");
         }
     }
 }
